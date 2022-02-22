36784843fb7b7cb8d1a90ab26e10457575ce68c9
Laura Luan <luan@us.ibm.com>
2022-02-22 16:12:02 -0500

    add printout for access error to private registry

--- pkg/imagefs/imagefs.go
+++ pkg/imagefs/imagefs.go
@@ -16,7 +16,7 @@ import (
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/google/go-containerregistry/pkg/v1/tarball"
 	"github.com/pkg/errors"
-	"github.com/tap8stry/orion/pkg/icrcredhelper"
+	icrcredhelper "github.com/tap8stry/orion/pkg/credhelpers/icr"
 	"k8s.io/release/pkg/spdx"
 )
 
@@ -118,6 +118,9 @@ func getImageReferences(imageName string) ([]struct {
 	if icrcredhelper.IsICRRegistry(ref.Name()) { //ibmcloud cr
 		icrHelper := icrcredhelper.NewICRCredentialsHelper()
 		descr, err = remote.Get(ref, remote.WithAuthFromKeychain(authn.NewKeychainFromHelper(icrHelper)))
+		if err != nil {
+			fmt.Printf("\nfailed in accessing ibmcloud icr. \n%s", icrcredhelper.Instruction)
+		}
 	} else {
 		descr, err = remote.Get(ref, remote.WithAuthFromKeychain(authn.DefaultKeychain))
 	}
